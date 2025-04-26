17d27eaebf5b052d168965883b437f334d208fd2
FelipeDrummond <felipe2002drummond@gmail.com>
2025-04-26 16:14:40 -0300

    changing enviroment

--- main.py
+++ main.py
@@ -1,15 +1,12 @@
 import gymnasium as gym
-from pettingzoo.mpe import simple_speaker_listener_v4
+from pettingzoo.mpe import simple_world_comm_v3
 import numpy as np
 import torch
-from models import SpeakerNetwork, ListenerNetwork
+
 
 # Create the environment
-env = simple_speaker_listener_v4.env(render_mode="human")
+env = simple_world_comm_v3.env(render_mode="human")
 
-# Initialize networks
-speaker_net = SpeakerNetwork()
-listener_net = ListenerNetwork()
 
 # Reset the environment
 env.reset()
@@ -29,14 +26,13 @@ for episode in range(1):  # Run 1 episode
             obs_tensor = torch.FloatTensor(observation)
             
             # Get action probabilities from appropriate network
-            if agent == 'speaker_0':
-                action_probs = speaker_net(obs_tensor)
-            else:  # listener_0
-                action_probs = listener_net(obs_tensor)
-            
+            if agent == 'leader':
+                action_probs = leader_net(obs_tensor)
+            else:
+                action_probs = follower_net(obs_tensor)
+                
             # Sample action from probabilities
             action = torch.multinomial(action_probs, 1).item()
-        
         # Step the environment
         env.step(action)
     
